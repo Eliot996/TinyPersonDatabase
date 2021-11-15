e9e136c3cee3fe259ad5090f2b4fb719d199ce42
Eliot996 <math10t6@stud.kea.dk>
2021-11-15 12:09:32 +0100

    constructor and toString was added

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,17 @@
 public class Person implements Comparable{
 
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return lastName + ' ' + firstName;
+    }
 
     @Override
     public int compareTo(Object o) {
